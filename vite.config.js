10639649fa3aac9538d3cea5888e7bb05c8e10de
jrpdango <72563699+jrpdango@users.noreply.github.com>
2024-06-01 15:20:24 +0800

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    // Dev config
+    return {
+      plugins: [react()]
+    };
+  } else {
+    // Prod config
+    return {
+      base: '/gittop/',
+      plugins: [react()]
+    };
+  }
+});
